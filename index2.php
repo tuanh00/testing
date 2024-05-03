86f014d3aae214c029bfc3e8c245a7fef63cdd04
Tu Anh <june265zz@gmail.com>
2024-05-03 11:56:16 -0400

    update index2.php

--- index2.php
+++ index2.php
@@ -1,5 +1,8 @@
 <?php
 
-echo "testing to check the commit working????";
-
+echo "<h1>Deliciousness in every bite!</h1>";
+echo "<p>Every cookie made at the Kevin Cookie company is crafted with inly the finest ingredients. 
+	Butter from grass fed cows. Unbleached flour with best flavor and textures. Organic sugar
+	only from India. Eggs from chickens that are free range. Even our water is special. It comes from the 	Cascade Mountain springs that we hike up to retrieve. Baking is our passion and we take it very
+	very seriously.</p>";
 ?>
